fb8c2baf31902913f60f9673d4caa1595742a14a
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-03 15:56:37 -0600

    Working on AJAX and server code

--- routes/routes.js
+++ routes/routes.js
@@ -35,8 +35,6 @@ router.post('/registration', function(req, res){
   });
 });
 
-//PHIL SECTION
-
 // // add pet to table in database
 // router.post('/addPet', function(req, res){
 //   pool.connect(function(errorConnectingToDatabase, client, done){
@@ -56,99 +54,36 @@ router.post('/registration', function(req, res){
 //     }
 //   });
 // });
-
-
-//END PHIL SECTION
-
-//KRIS SECTION
-// // update pet in pets table in database
-// router.put('/updatePet', function(req, res){
-//   pool.connect(function(errorConnectingToDatabase, client, done){
-//     if(errorConnectingToDatabase) {
-//       console.log('Error connecting to database: ', errorConnectingToDatabase);
-//       res.sendStatus(500);
-//     } else {
-//       client.query('SELECT * FROM "books";', function(errorMakingQuery, result){
-//         done();
-//         if(errorMakingQuery) {
-//           console.log('Error making the database query: ', errorMakingQuery);
-//           res.sendStatus(500);
-//         } else {
-//           res.send(result.rows);
-//         }
-//       });
-//     }
-//   });
-// });
 //
-// // delete pet from pets table in database
-// router.delete('/deletePet', function(req, res){
-//   pool.connect(function(errorConnectingToDatabase, client, done){
-//     if(errorConnectingToDatabase) {
-//       console.log('Error connecting to database: ', errorConnectingToDatabase);
-//       res.sendStatus(500);
-//     } else {
-//       client.query('SELECT * FROM "books";', function(errorMakingQuery, result){
-//         done();
-//         if(errorMakingQuery) {
-//           console.log('Error making the database query: ', errorMakingQuery);
-//           res.sendStatus(500);
-//         } else {
-//           res.send(result.rows);
-//         }
-//       });
-//     }
-//   });
-// });
-
-//END KRIS SECTION
-
-// TOM SECTION
+// //
 
 
-// END TOM SECTION
 
-//SAM SECTION
 
-//END SAM SECTIOn
-// //sends current date to database when pet is checked in
-// router.post('/checkIn', function(req, res){
-//   pool.connect(function(errorConnectingToDatabase, client, done){
-//     if(errorConnectingToDatabase) {
-//       console.log('Error connecting to database: ', errorConnectingToDatabase);
-//       res.sendStatus(500);
-//     } else {
-//       client.query('SELECT * FROM "books";', function(errorMakingQuery, result){
-//         done();
-//         if(errorMakingQuery) {
-//           console.log('Error making the database query: ', errorMakingQuery);
-//           res.sendStatus(500);
-//         } else {
-//           res.send(result.rows);
-//         }
-//       });
-//     }
-//   });
-// });
-//
-// //sends current date to database when pet is checked out
-// router.post('/checkOut', function(req, res){
-//   pool.connect(function(errorConnectingToDatabase, client, done){
-//     if(errorConnectingToDatabase) {
-//       console.log('Error connecting to database: ', errorConnectingToDatabase);
-//       res.sendStatus(500);
-//     } else {
-//       client.query('SELECT * FROM "books";', function(errorMakingQuery, result){
-//         done();
-//         if(errorMakingQuery) {
-//           console.log('Error making the database query: ', errorMakingQuery);
-//           res.sendStatus(500);
-//         } else {
-//           res.send(result.rows);
-//         }
-//       });
-//     }
-//   });
-// });
+//Kris's section starts here//
+router.put('/updatePet/:id', function(req, res){
+  var petID = req.params.id;
+  var petObject = req.body;
+  console.log('id of pet to update: ', petID);
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      client.query('UPDATE pets SET name=$1, breed=$2, color=$3 WHERE id=$4;',
+      [petObject.petName, petObject.breed, petObject.color, bookId],
+      function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(202);
+        }
+      });
+    }
+  });
+}); // closing put request
 
+// Kris's section ends here //
 module.exports = router;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,75 +1,113 @@
 console.log(new Date().getFullYear() + " Javascript sourced");
 console.log('JS Sourced');
 $(function(){
-console.log('jQuery Sourced');
+  console.log('jQuery Sourced');
 
-// SAM
+  // SAM
 
-// $.ajax({
-// type: 'GET',
-// url: '/pets',
-// success: function(response) {
-//     console.log('/pets response: ', response);
-// },
-// error: function(error){
-//   console.log('Ajax-GET failed', error);
-// }
-// }) // NOTE: FOR: Ajax-GET
-
-
-//END SAM SECTIOn
-//////////////// NOTE: Owner Name
-// function for ajax POST
-$('#registerButton').on('click', function(){
-  console.log('#registerButton clicked');
-var newOwner = {
-  firstName: $('#ownerFirstNameInput').val(),
-  lastName: $('#ownerLastNameInput').val()
-}
-$.ajax({
-  type: 'POST',
-  url:'pets/registration',
-  data: newOwner,
-  success: function(response){
-    console.log('ajax-registration: ', response);
-  },
-  error: function(response){
-    console.log('ajax-registration: ', response);
-  }
-})
-});
-//PHIL SECTION
-
-// NOTE: FOR: $('#registerButton').on('click', function(){
-//////////////// NOTE: Pet Details
-// $('#addPetButton').on('click', function(){
-//   console.log('#addPetButton clicked');
-// var newPet = {
-//   pet: $('petNameInput').val(),
-//   breed: $('petBreedInput').val(),
-//   color: $('petColorInput').val()
-// }
-// $.ajax({
-//   type: 'POST',
-//   url: '/addPet',
-//   data: newPet;
-
-// )} // NOTE: FOR: $('#addPetButton').on('click', function(){
-// // TODO: function for ajax POST
-// // $.ajax({
-// //   type: 'POST'
-// //
-// //
-// // })
-
-//END PHIL SECTION
-
-//KRIS SECTION
+  // $.ajax({
+  // type: 'GET',
+  // url: '/pets',
+  // success: function(response) {
+  //     console.log('/pets response: ', response);
+  // },
+  // error: function(error){
+  //   console.log('Ajax-GET failed', error);
+  // }
+  // }) // NOTE: FOR: Ajax-GET
 
 
-//END KRIS SECTION
+  //END SAM SECTIOn
+  //////////////// NOTE: Owner Name
+  // function for ajax POST
+  $('#registerButton').on('click', function(){
+    console.log('#registerButton clicked');
+    var newOwner = {
+      firstName: $('#ownerFirstNameInput').val(),
+      lastName: $('#ownerLastNameInput').val()
+    }
+    $.ajax({
+      type: 'POST',
+      url:'pets/registration',
+      data: newOwner,
+      success: function(response){
+        console.log('ajax-registration: ', response);
+      },
+      error: function(response){
+        console.log('ajax-registration: ', response);
+      }
+    })
+  });
+  //PHIL SECTION
 
-//TOM SECTION
+  // NOTE: FOR: $('#registerButton').on('click', function(){
+  //////////////// NOTE: Pet Details
+  // $('#addPetButton').on('click', function(){
+  //   console.log('#addPetButton clicked');
+  // var newPet = {
+  //   pet: $('petNameInput').val(),
+  //   breed: $('petBreedInput').val(),
+  //   color: $('petColorInput').val()
+  // }
+  // $.ajax({
+  //   type: 'POST',
+  //   url: '/addPet',
+  //   data: newPet;
 
-//END TOM SECTION
+  // )} // NOTE: FOR: $('#addPetButton').on('click', function(){
+  // // TODO: function for ajax POST
+  // // $.ajax({
+  // //   type: 'POST'
+  // //
+  // //
+  // // })
+
+  //END PHIL SECTION
+
+  //KRIS SECTION
+  $('.updateButton').on('click', function(){
+    var petNameToSave = $(this).parent().prev().prev().prev().find('.petsDisplayName').val();
+    var petBreedToSave = $(this).parent().prev().prev().find('.petsDisplayBreed').val();
+    var petColorToSave = $(this).parent().prev().find('.petsDisplayColor').val();
+    var petID = $(this).parent().parent().data('petID');
+
+    var petUpdateObject = {
+      petName: petNameToSave,
+      breed: petBreedToSave,
+      color: petColorToSave,
+      petID: petID
+    };
+    $.ajax({
+      type: 'PUT',
+      url:'pets/updatePet' + petUpdateObject,
+      data: petUpdateObject,
+      success: function(response){
+        console.log('ajax-registration: ', response);
+      },
+      error: function(response){
+        console.log('ajax-registration: ', response);
+      }
+    })
+  }); // end of update button click listener
+
+  $('.deleteButton').on('click', function(){
+    var petIDToDelete = $(this).parent().parent().data('petID');
+    $.ajax({
+      type: 'DELETE',
+      url:'pets/deletePet' + petIDToDelete,
+      success: function(response){
+        console.log('ajax-registration: ', response);
+      },
+      error: function(response){
+        console.log('ajax-registration: ', response);
+      }
+    })
+  }); // end of delete button click listener
+
+
+  //END KRIS SECTION
+
+  //TOM SECTION
+
+  //END TOM SECTION
 }); // NOTE: FOR: $(function(){
